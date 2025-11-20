aaca6b66697b5c3d7ad2d6c7a79c425a16263bd8
Steven L. Speek <slspeek@gmail.com>
2025-11-20 14:13:13 +0100

    prepare-education-box.sh: extract gnome-cursus.zip in Documenten

--- bin/prepare-education-box.sh
+++ bin/prepare-education-box.sh
@@ -7,11 +7,13 @@ GITHUB_RELEASES=https://github.com/$GITHUB_USER/$GITHUB_REPO_NAME/releases/lates
 
 cd ~/Downloads
 wget $GITHUB_RAW/resources/bloemen.zip 
-wget $GITHUB_RELEASES/samenvatting.pdf
-wget $GITHUB_RELEASES/verder-leren.pdf
+wget $GITHUB_RELEASES/gnome-cursus.zip
 
 cd ~/Afbeeldingen
 unzip ~/Downloads/bloemen.zip
+
+cd ~/Documenten
+unzip ~/Downloads/gnome-cursus.zip
 
 sudo apt-get -y remove chromium
 if which google-chrome; then sudo apt-get remove -y google-chrome-stable; fi
