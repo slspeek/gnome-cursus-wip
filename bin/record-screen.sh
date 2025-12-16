b34c5e4ae6251f1f6fbdf951db76d9dd103f3bc8
Steven L. Speek <slspeek@gmail.com>
2025-12-16 16:15:40 +0100

    wip: record-screen.sh

--- bin/record-screen.sh
+++ bin/record-screen.sh
@@ -1,1 +1,5 @@
-recordmydesktop --no-sound --display :0 --width 3840 --height 2160 &> $HOME/recording.log
+#!/bin/bash
+
+OUTFILE=$HOME/screenrecording-$(date +%Y-%m-%d-%H%m).ogv
+recordmydesktop --no-sound --display :0 --width 3840 --height 2160 -o $OUTFILE  &> $HOME/recording.log
+notify-send Finished recording $OUTFILE
